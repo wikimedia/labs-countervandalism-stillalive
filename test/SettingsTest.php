5d1fddb0679cf6d8accfb75bd496b5fa9a9b4642
Timo Tijhof <krinklemail@gmail.com>
2016-11-07 20:51:48 +0000

    Remove unused localSettings-cvn.json
    Support for JSON remains for the time being, but no longer
    used on any CVN nodes. Remove maintenance of two config files.

--- test/SettingsTest.php
+++ test/SettingsTest.php
@@ -3,25 +3,10 @@
 require_once dirname( __DIR__ ) . '/Util.php';
 
 class SettingsTest extends PHPUnit_Framework_TestCase {
-	public function testCvnSettingsJSON() {
-		$localSettings = Util::loadConfig( dirname( __DIR__ ) . '/localSettings-cvn.json' );
-
-		$this->assertEquals( JSON_ERROR_NONE, json_last_error(), 'last error' );
-		$this->assertEquals( 'array', gettype( $localSettings ), 'parsed without errors' );
-		$this->assertEquals( 'cvn.cvnservice', $localSettings['user'], 'read property' );
-	}
-
-	public function testCvnSettingsYAML() {
+	public function testCvnSettings() {
 		$localSettings = Util::loadConfig( dirname( __DIR__ ) . '/localSettings-cvn.yaml' );
 
 		$this->assertEquals( 'array', gettype( $localSettings ), 'parsed without errors' );
 		$this->assertEquals( 'cvn.cvnservice', $localSettings['user'], 'read property' );
-	}
-
-	public function testCvnSettings() {
-		$settingJSON = Util::loadConfig( dirname( __DIR__ ) . '/localSettings-cvn.json' );
-		$settingsYAML = Util::loadConfig( dirname( __DIR__ ) . '/localSettings-cvn.yaml' );
-
-		$this->assertEquals( $settingJSON, $settingsYAML, 'settings formats in sync' );
 	}
 }
